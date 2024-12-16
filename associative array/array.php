c7ee00eda77dedb07aaa7da1d4f2347bd5b7f9a6
abdullad2bd <abdullahalmaruf2bd@gmail.com>
2024-12-16 20:42:50 +0600

    first commit

---
+++ associative array/array.php
@@ -0,0 +1,44 @@
+<?php
+
+// PHP 
+// Array
+// Associative Array 
+
+
+
+// Index array 
+ 
+// $lis = array("Volvo", "BMW", "Toyota");
+// echo $lis[0];
+
+// $cars = array("Volvo", "BMW", "Toyota");
+// echo $cars[0];
+
+
+// $cars = array("Volvo", "BMW", "Toyota");
+// $cars[1] = "Ford";
+// var_dump($cars);
+
+
+
+// $cars = array("Volvo", "BMW", "Toyota");
+// foreach ($cars as $x) {
+//   echo "$x <br>";
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
