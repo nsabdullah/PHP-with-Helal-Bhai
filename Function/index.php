a92f35d7c9305df6c11833c6218400ce732e44ab
abdullad2bd <abdullahalmaruf2bd@gmail.com>
2024-12-17 18:06:32 +0600

    function

---
+++ Function/index.php
@@ -0,0 +1,19 @@
+<?php
+// Function
+// Let's Start
+
+
+// Function without argument
+function sayHello(){
+ echo "Hello there! <br/>" ;
+};
+sayHello();
+
+
+
+//Function with argument
+function sayGoogMorning( string $name){
+    echo "Good mornig " . $name . " ! ";
+}
+sayGoogMorning("Jubayer")
+?>
