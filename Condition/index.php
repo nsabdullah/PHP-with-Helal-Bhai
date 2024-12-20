91d8ba55134414e27e0b3da7dab7fd11baa46e79
abdullad2bd <abdullahalmaruf2bd@gmail.com>
2024-12-20 16:52:14 +0600

    condition

---
+++ Condition/index.php
@@ -0,0 +1,27 @@
+<?php
+
+
+$userAge = 40;
+
+
+
+
+// if($userAge == 20){
+//   echo "User age is 20";
+// }elseif($userAge == 30){
+//    echo "User age is 30";
+// }elseif($userAge == 40){
+//    echo "User age is 40";
+// }else{
+//   echo "User age is Undefined";
+// };
+
+
+
+// multiple value check
+// if($userAge > 20 || $userAge < 30){
+//   echo "You are accepted!";
+// };
+
+// localhost/PHP with Helal Bhai/Condition/index.php
+?>
