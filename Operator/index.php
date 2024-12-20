c421a0144376d34df01e7ec76331c963dd08d1f9
abdullad2bd <abdullahalmaruf2bd@gmail.com>
2024-12-20 16:36:18 +0600

    operator

---
+++ Operator/index.php
@@ -0,0 +1,49 @@
+<?php
+
+
+$a = "I ";
+$b = "Like ";
+$c = "to ";
+$d = "write ";
+$e = "PHP ";
+$f = "code🥱";
+$sum = $a . $b . $c;
+// $sum = $a + $b + $c;
+// echo  $sum;
+
+// echo $a;
+// echo $b;
+// echo $c;
+// echo $d;
+// echo $e;
+// echo $f;
+
+$g = 10;
+$h = 11;
+$i = 12;
+
+// echo $g + $h;
+// echo $g . $h;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// localhost/PHP with Helal Bhai/Operator/index.php
+?>
+
