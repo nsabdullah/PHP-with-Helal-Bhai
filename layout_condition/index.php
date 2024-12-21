3476ba3e0a86b890a433c095509f3f9e63cb72c8
abdullad2bd <abdullahalmaruf2bd@gmail.com>
2024-12-21 15:36:40 +0600

    layout

---
+++ layout_condition/index.php
@@ -0,0 +1,27 @@
+<?php
+
+include('layout/header.php');
+ echo   '<h1> This is home page</h1>';
+include('layout/footer.php');
+
+/* redirect */
+// $isLogged = false;
+// if(!$isLogged){
+//   header('Location: layout_condition/logIn.php');
+// }
+
+
+
+
+
+$isLoggedin = true;
+
+if(!$isLoggedin) {
+  header('Location: layout_condition/logIn.php');
+}
+
+
+
+
+// localhost/PHP with Helal Bhai/layout_condition/index.php
+?>
