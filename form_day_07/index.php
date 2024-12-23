b77bc5bca37d79af9015982e703754687844c5c2
abdullad2bd <abdullahalmaruf2bd@gmail.com>
2024-12-23 21:12:35 +0600

    form

---
+++ form_day_07/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+
+
+
+<form>
+  <label for="name">Name:</label><br>
+  <input type="text" id="name" name="name"><br>
+  <label for="email">Email:</label><br>
+  <input type="text" id="email" name="email"><br>
+  <label for="password">Password:</label><br>
+  <input type="password" id="password" name="password"><br>
+  <label for="comment">Comment:</label><br>
+  <textarea id="comment" name="comment"></textarea><br>
+  <input type="submit" value="Submit">
+</form>
+
+
+
+
+
+<!--  localhost/PHP with Helal Bhai/form_day_07/index.php -->
+
+  
+</body>
+</html>
+
