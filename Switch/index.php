5d63660c7c60a3219f9b2692181a4a43f9b44c46
abdullad2bd <abdullahalmaruf2bd@gmail.com>
2024-12-23 18:15:01 +0600

    switch

--- Switch/index.php
+++ Switch/index.php
@@ -1,7 +1,39 @@
 <?php
+$day = "Monday";
+
+switch ($day) {
+    case "Monday":
+        echo "Today is the first day of the work week.";
+        break;
+    case "Tuesday":
+        echo "It's the second day of the work week.";
+        break;
+    case "Wednesday":
+        echo "We're halfway through the week!";
+        break;
+    case "Thursday":
+        echo "Just one more day until Friday!";
+        break;
+    case "Friday":
+        echo "It's the last working day of the week!";
+        break;
+    case "Saturday":
+    case "Sunday":
+        echo "It's the weekend! Time to relax!";
+        break;
+    default:
+        echo "Invalid day.";
+        break;
+}
 
 
 
 
-// localhost/PHP with Helal Bhai/Switch/index.php
+
+
+
+
+
+
+
 ?>
