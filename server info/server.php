3f404282349ceb31bada4ab7f25471fb6b611ec7
abdullad2bd <abdullahalmaruf2bd@gmail.com>
2024-12-23 18:29:24 +0600

    server

--- server info/server.php
+++ server info/server.php
@@ -1,14 +1,18 @@
 <?php
-
 $server = $_SERVER;
-
-
 echo "<pre>";
 // var_dump($server)
 // echo $server[0];
 
 echo $server["HTTP_HOST"];
-  
-echo $server ["PATH"]
+echo $server ["PATH"];
+
+
+
+
+
+
+
+
 // localhost/PHP with Helal Bhai/server info/server.php
 ?>
