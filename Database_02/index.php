e5a795f61697c94d120e2cde5ab2818217658748
abdullad2bd <abdullahalmaruf2bd@gmail.com>
2025-01-07 20:02:15 +0600

    update

--- Database_02/index.php
+++ Database_02/index.php
@@ -1,25 +1,24 @@
 <?php
+echo '<pre>';
 // connection with database
 $serverName = "localhost";
 $userNAme = "root";
 $password = "";
+$dbName = "users";
 
 // connection 
-$connection = mysqli_connect($serverName, $userNAme, $password);
-
+$connection = mysqli_connect($serverName, $userNAme, $password , $dbName);
 if($connection){
   echo "Database connected";
 }
+$sql = "INSERT INTO submitted (name, email, password) VALUES ('Abdullah', 'dsgth@gmail.com' 'srtyhs')";
+ 
 
-
-
-
-
-
-
-
-
-
-
+$insert = mysqli_query($connection, $sql);
+if($insert){
+  echo "Data inserted";
+}else{  
+  echo "Data not inserted";
+}
 
 ?>
